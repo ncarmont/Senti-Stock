370744af2e82c2eca937b467486df3d52c9736ce
Kshitij Ranganath <kshitijvr93@gmail.com>
2019-03-30 20:46:30 -0400

    basic insert works

--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import json
 import datetime                       
 
 from flask import Flask, redirect, url_for,render_template,request
-from bson import ObjectId
-from pymongo import MongoClient
+
+from flask_pymongo import PyMongo
+# from bson import ObjectId
+# from pymongo import PyMongo
 
 app = Flask(__name__)
-# client = MongoClient("mongodb://127.0.0.1:27017") #host uri
-# db = client.mymongodb #Select the database
-# # mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mongotest"
+mongo = PyMongo(app)
 
 @app.route("/")
 def home():
@@ -20,8 +21,25 @@ def home():
 def func1():
     if request.form:
         # redirect(url_for('page'))
+        print(request.form)
+        dict1 = request.form.to_dict()
+        company = dict1["company"]
+        collec = mongo.db.testcollec
+        collec.insert({'company_name':company})
         return render_template("graphs.html", var = request.form)
     return render_template("home.html")
+
+@app.route("/add")
+def add(form_data):
+    collec = mongo.db.testcollec
+    collec.insert({'name':'Apple' , 'cost' : '1000'})
+    return("added company")
+
+# @app.route("/user/<companyname>")
+# def user_profile(username):
+#     user = mongo.db.users.find_one_or_404({"_id": companyname})
+#     return render_template("user.html",
+#         user=user)
   
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080,debug=True)
